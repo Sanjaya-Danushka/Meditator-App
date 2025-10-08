6b39bd75dd00023850e701a045aad759d212497f
Sanjaya Danushka <dsanjaya712@gmail.com>
2025-10-08 22:51:55 +0530

    mindfull-exercise page created

--- lib/pages/mindfull_exercise_details_page.dart
+++ lib/pages/mindfull_exercise_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meditator/models/mindfull_exercisemodel.dart';
 import 'package:meditator/utils/colors.dart';
+import 'package:meditator/utils/text_styles.dart';
 
 class MindfullExerciseDetailsPage extends StatelessWidget {
   final MindfullExerciseModel exercise;
@@ -16,6 +17,46 @@ class MindfullExerciseDetailsPage extends StatelessWidget {
             fontSize: 29,
             fontWeight: FontWeight.bold,
             color: AppColors.primaryPurple,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(exercise.name, style: AppTextStyles.titleStyle),
+              SizedBox(height: 16),
+              Text(exercise.category),
+              SizedBox(height: 16),
+              Text(exercise.description, textAlign: TextAlign.justify),
+              SizedBox(height: 16),
+              Text("Instructions:", style: AppTextStyles.titleStyle),
+              SizedBox(height: 16),
+              ...exercise.instructions.map(
+                (instruction) => Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("•  ", style: TextStyle(fontSize: 16)),
+                      Expanded(
+                        child: Text(instruction, textAlign: TextAlign.justify),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Icon(Icons.timer, color: AppColors.primaryGreen),
+                  SizedBox(width: 8),
+                  Text("${exercise.duration} min"),
+                ],
+              ),
+            ],
           ),
         ),
       ),
